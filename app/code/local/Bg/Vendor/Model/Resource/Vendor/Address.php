18cc98781857e90b094db9908e8a0ba7a912a918
Bhargav5496 <gohelbhargav442@gmail.com>
2023-05-23 13:24:10 +0530

    vendor setup completed.

---
+++ app/code/local/Bg/Vendor/Model/Resource/Vendor/Address.php
@@ -0,0 +1,18 @@
+<?php
+
+class Bg_Vendor_Model_Resource_Vendor_Address extends Mage_Core_Model_Resource_Db_Abstract
+{
+    protected $primaryKey = 'vendor_id';
+
+    protected function _construct()
+    {  
+        $this->_init('vendor/vendor_address', $this->primaryKey);
+    }  
+
+    public function setPrimaryKey($primaryKey)
+    {
+        $this->primaryKey = $primaryKey;
+        $this->_init('vendor/vendor_address', $primaryKey);
+        return $this;
+    }
+}
