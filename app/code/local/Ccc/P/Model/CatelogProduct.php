2f4178dbe91ecdfe7745c370fafc009e5b271a69
Bhargav5496 <gohelbhargav442@gmail.com>
2023-05-25 09:48:15 +0530

    task completed till 19

--- app/code/local/Ccc/P/Model/CatelogProduct.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-/**
- * 
- */
-class Ccc_P_Model_CatalogProduct extends Mage_Catalog_Model_Product
-{
-}
---
+++ app/code/local/Ccc/Vendor/controllers/AdminHtml/Catalog/ProductController.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'C:\xampp\htdocs\2023\magento\magento-mirror\app\code\core\Mage\Adminhtml\controllers\Catalog\ProductController.php';
+
+class Ccc_Vendor_Adminhtml_Catalog_ProductController extends Mage_Adminhtml_Catalog_ProductController
+{ 
+    public function indexAction()
+    {
+        echo "Vendor vala productController...";       
+    }   
+}
+
